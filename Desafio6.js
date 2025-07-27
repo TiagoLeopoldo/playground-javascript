49cda5004e56382ca4f216b70da61a0203c9f022
TiagoLeopoldo <tnleopoldo.dev@gmail.com>
2025-07-27 20:15:44 -0300

    feat:(não resolvido) adiciona Desafio6.js ao repositório

---
+++ Desafio6.js
@@ -0,0 +1,17 @@
+// 🔍 Desafio 6.0 — Transformação com .map()
+// Você receberá um array de nomes em caixa alta (tipo gritaria: "TIAGO", "ANA", "MARCOS").
+// O objetivo é devolver um novo array com os nomes em caixa baixa e capitalizados, como "Tiago", "Ana", "Marcos".
+
+
+// ✅ Requisitos:
+// - Usar .map() para iterar sobre o array.
+// - Transformar cada nome em:
+// - Primeira letra maiúscula
+// - O resto em minúsculas
+// - Sem alterar o array original.
+// - Devolver array transformado com return ou console.log() claro.
+
+// 💡 Exemplo:
+// const nomes = ["TIAGO", "ANA", "MARCOS"];
+// // resultado desejado: ["Tiago", "Ana", "Marcos"]
+
