fa95e5948d9218a62baedef32451452cdda23478
TiagoLeopoldo <tnleopoldo.dev@gmail.com>
2025-07-27 20:14:21 -0300

    feat: adiciona resolução do Desafio5.js

--- Desafio5.js
+++ Desafio5.js
@@ -18,3 +18,62 @@
 
 // 💡 Dica ninja:
 // Tente evitar um excesso de if...else, e use retorno antecipado, operadores lógicos e expressões limpas para deixar o código elegante.
+
+const checkAgeAccess = (age, hasPermission) => {
+  if (typeof age === "number") {
+    if (age < 18) {
+      if (hasPermission === false) {
+     console.log("Acesso negado.");
+      } else if (hasPermission === true) {
+      console.log("Acesso liberado com permissão especial.");
+      }
+    } else if (age >= 18 && age < 65) {
+     console.log("Acesso liberado.");
+    } else if (age >= 65) {
+     console.log("Acesso liberado para sênior");
+    }
+  } else {
+      console.log("Idade inválida.");
+  }
+ 
+};
+
+checkAgeAccess(15, false);
+checkAgeAccess("12", true);
+checkAgeAccess(13, true);
+checkAgeAccess(67);
+checkAgeAccess("35");
+checkAgeAccess(54);
+
+
+// Versão refatorada, código mais limpo.
+
+const checkAgeAccessTwo = (age, hasPermission) => {
+  if (typeof age != "number") {
+    console.log("Idade inválida.");
+    return 
+  }
+
+  if (age < 18) {
+    console.log(hasPermission ? "Acesso liberado com permissão especial." : "Acesso negado.");
+    return
+  }
+
+  if (age >= 18 && age < 65) {
+    console.log("Acesso liberado.");
+    return
+  }
+
+  if (age >= 65) {
+    console.log("Acesso liberado para sênior.");
+    return
+  }
+
+};
+
+checkAgeAccessTwo(15, false);
+checkAgeAccessTwo("12", true);
+checkAgeAccessTwo(13, true);
+checkAgeAccessTwo(67);
+checkAgeAccessTwo("35");
+checkAgeAccessTwo(54);
